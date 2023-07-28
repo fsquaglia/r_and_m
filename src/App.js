6244edf188800484fe5411db49ffee8fc0b065b2
fsquaglia <fernandosquaglia@gmail.com>
2023-07-28 12:17:24 -0300

    hw08a

--- src/App.js
+++ src/App.js
@@ -2,29 +2,44 @@ import './App.css';
 import Nav from './components/Nav.jsx';
 import Cards from './components/Cards.jsx';
 import { useState } from 'react';
+import axios from 'axios';
 
 
 function App() {
 
    const [characters, setCharacters] = useState([]);
 
+   function onSearch(id) {
+      axios(`https://rickandmortyapi.com/api/character/${id}`).then(({ data }) => {
+         if (data.name) {
+            setCharacters((oldChars) => [...oldChars, data]);
+         } else {
+            console.error()
+            window.alert('¡No hay personajes con este ID!');
+         }
+      });
+   }
 
-   function onSearch () {
-      const example = {
-         id: 1,
-         name: 'Rick Sanchez',
-         status: 'Alive',
-         species: 'Human',
-         gender: 'Male',
-         origin: {
-            name: 'Earth (C-137)',
-            url: 'https://rickandmortyapi.com/api/location/1',
-         },
-         image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
-      };
-      const nuevoArray = [...characters, example]
+   //mi codigo anterior
+   // function onSearch (id) {
+   //    const example = {
+   //       id: 1,
+   //       name: 'Rick Sanchez' + id,
+   //       status: 'Alive',
+   //       species: 'Human',
+   //       gender: 'Male',
+   //       origin: {
+   //          name: 'Earth (C-137)',
+   //          url: 'https://rickandmortyapi.com/api/location/1',
+   //       },
+   //       image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
+   //    };
+   //    const nuevoArray = [...characters, example]
+   //     setCharacters(nuevoArray)
+   // };
+
+   function onClose (id) {
       
-      setCharacters(nuevoArray)
    };
 
 
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,8 +1,17 @@
+import { useState } from "react";
+
 export default function SearchBar(props) {
+   const [id, setId] = useState ("")
+   
+   function handleChange (event) {
+      setId (event.target.value)
+      console.log(event.target.value)
+   };
+   let funcion = props.onSearch
    return (
       <div>
-         <input type='search' />
-         <button onClick={props.onSearch}>Agregar</button>
+         <input type='search' onChange={handleChange}/>
+         <button onClick={()=>funcion(id)}>Agregar</button>
       </div>
    );
 }
